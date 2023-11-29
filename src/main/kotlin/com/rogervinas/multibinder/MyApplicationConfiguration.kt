42f67f85dedf1ced7bf06ac6bb01e25eb25a74fe
Roger Viñas <roger.vinas@fastzink.com>
2023-11-29 20:33:25 +0100

    Go for lambdas

--- src/main/kotlin/com/rogervinas/multibinder/MyApplicationConfiguration.kt
+++ src/main/kotlin/com/rogervinas/multibinder/MyApplicationConfiguration.kt
@@ -4,9 +4,7 @@ import org.apache.kafka.streams.kstream.KStream
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
-import java.util.function.Consumer
 import java.util.function.Function
-import java.util.function.Supplier
 
 @Configuration
 class MyApplicationConfiguration {
@@ -15,13 +13,14 @@ class MyApplicationConfiguration {
   fun textStreamProducer() = TextStreamProducer()
 
   @Bean
-  fun textProducer(textStreamProducer: TextStreamProducer): Supplier<Flux<TextEvent>> = textStreamProducer
+  fun textProducer(textStreamProducer: TextStreamProducer): () -> Flux<TextEvent> = textStreamProducer::get
 
   @Bean
   fun textLengthProcessor(): Function<KStream<String, TextEvent>, KStream<String, LengthEvent>> = TextLengthProcessor()
 
   @Bean
-  fun lengthConsumer(lengthProcessor: LengthProcessor): Consumer<LengthEvent> = LengthStreamConsumer(lengthProcessor)
+  fun lengthConsumer(lengthProcessor: LengthProcessor): (LengthEvent) -> Unit =
+    LengthStreamConsumer(lengthProcessor)::accept
 
   @Bean
   fun lengthProcessor() = LengthConsoleProcessor()
