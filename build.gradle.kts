191fa4c8c786e1e7e6426593a8eda93d5fb683af
Roger Viñas <roger.vinas@fastzink.com>
2023-11-29 20:37:09 +0100

    Spring Boot 3.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,21 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "2.7.18"
-  id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  id("org.springframework.boot") version "3.2.0"
+  id("io.spring.dependency-management") version "1.1.4"
   kotlin("jvm") version "1.9.21"
   kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "com.rogervinas"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
   mavenCentral()
 }
 
-val springCloudVersion = "2021.0.8"
+val springCloudVersion = "2023.0.0-RC1"
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -43,7 +43,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
   kotlinOptions {
     freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "17"
+    jvmTarget = "21"
   }
 }
 
