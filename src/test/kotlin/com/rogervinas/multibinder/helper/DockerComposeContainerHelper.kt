6faa7a53ad61b57dc7aa19b2ed7761ff85bebe25
Roger Vinas <roger.vinas@fastzink.com>
2024-06-19 17:46:03 +0200

    Use confluent containers

--- src/test/kotlin/com/rogervinas/multibinder/helper/DockerComposeContainerHelper.kt
+++ src/test/kotlin/com/rogervinas/multibinder/helper/DockerComposeContainerHelper.kt
@@ -7,27 +7,31 @@ import org.testcontainers.containers.wait.strategy.WaitAllStrategy
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy.Mode.WITH_INDIVIDUAL_TIMEOUTS_ONLY
 import java.io.File
 
-private const val KAFKA = "kafka"
-private const val KAFKA_PORT = 9094
-private const val ZOOKEEPER = "zookeeper"
-private const val ZOOKEEPER_PORT = 2181
-
 class DockerComposeContainerHelper {
 
-  fun createContainer(): ComposeContainer = ComposeContainer(File("docker-compose.yml"))
-    .withLocalCompose(true)
-    .withExposedService(
-      KAFKA,
-      KAFKA_PORT,
-      WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-        .withStrategy(forListeningPort())
-        .withStrategy(forLogMessage(".*creating topics.*", 1))
-    )
-    .withExposedService(
-      ZOOKEEPER,
-      ZOOKEEPER_PORT,
-      WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-        .withStrategy(forListeningPort())
-        .withStrategy(forLogMessage(".*binding to port.*", 1))
-    )
+  companion object {
+    private const val KAFKA = "kafka"
+    private const val KAFKA_PORT = 9094
+    private const val ZOOKEEPER = "zookeeper"
+    private const val ZOOKEEPER_PORT = 2181
+  }
+
+  fun createContainer(): ComposeContainer {
+    return ComposeContainer(File("docker-compose.yml"))
+      .withLocalCompose(true)
+      .withExposedService(
+        KAFKA,
+        KAFKA_PORT,
+        WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+          .withStrategy(forListeningPort())
+          .withStrategy(forLogMessage(".*started.*", 1))
+      )
+      .withExposedService(
+        ZOOKEEPER,
+        ZOOKEEPER_PORT,
+        WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+          .withStrategy(forListeningPort())
+          .withStrategy(forLogMessage(".*Started.*", 1))
+      )
+  }
 }
