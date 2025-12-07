4c877ce5926e8b581a128def57b2eefccdf78706
Roger Vinas <roger.vinas@fastzink.com>
2025-12-07 18:55:30 +0100

    Remove workaround

--- src/main/kotlin/com/rogervinas/multibinder/MyApplicationConfiguration.kt
+++ src/main/kotlin/com/rogervinas/multibinder/MyApplicationConfiguration.kt
@@ -1,11 +1,8 @@
 package com.rogervinas.multibinder
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.kafka.streams.kstream.KStream
-import org.springframework.cloud.function.json.JacksonMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import reactor.core.publisher.Flux
 import java.util.function.Function
 
@@ -25,9 +22,4 @@ class MyApplicationConfiguration {
 
   @Bean
   fun lengthConsoleProcessor() = LengthConsoleProcessor()
-
-  // Workaround for https://github.com/spring-cloud/spring-cloud-function/issues/1158
-  // Introduced in spring-cloud-function 4.1.3 via spring-cloud-dependencies 2023.0.3
-  @Bean @Primary
-  fun jacksonMapper(objectMapper: ObjectMapper) = JacksonMapper(objectMapper)
 }
